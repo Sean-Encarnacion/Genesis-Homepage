07286a9f7d5ea1b17fde54605dea7168e7ae1f64
JoshMatthew <joshmatthewtalplacido@gmail.com>
2022-05-13 01:55:21 +0800

    fixed booking page

--- src/components/Booking/index.js
+++ src/components/Booking/index.js
@@ -1,53 +1,72 @@
-import React from 'react'
-import emailjs from "emailjs-com"
-import { Container, FormWrap, Icon, FormContent, Form, FormH1, FormLabel, FormSelect, FormInput, FormInputBtn, FormButton, Text } from './BookingElements'
+import React from "react";
+import emailjs from "emailjs-com";
+import {
+  Container,
+  FormWrap,
+  Icon,
+  FormContent,
+  Form,
+  FormH1,
+  FormLabel,
+  FormSelect,
+  FormInput,
+  FormInputBtn,
+  FormButton,
+  Text,
+} from "./BookingElements";
 
 const Booking = () => {
+  function sendEmail(e) {
+    e.preventDefault();
 
-    function sendEmail(e){
-        e.preventDefault();
-
-    emailjs.sendForm('gmail', 'template_c0857a4', e.target, '5skgWqC7tKNlBOnk2')
-      .then((result) => {
+    emailjs
+      .sendForm("gmail", "template_c0857a4", e.target, "5skgWqC7tKNlBOnk2")
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset()
-    };
+        }
+      );
+    e.target.reset();
+  }
   return (
     <>
-    <Container>
+      <Container>
         <FormWrap>
-            <Icon to="/">Genesis Farm</Icon>
-            <FormContent>
-                <Form action='' method='POST' onSubmit={sendEmail}>
-                    <FormH1>Reservation Form</FormH1>
-                    <FormLabel htmlFor='for'>First Name</FormLabel>
-                    <FormInput type='name' name='FN' required />
-                    <FormLabel htmlFor='for'>Last Name</FormLabel>
-                    <FormInput type='name' name='LN' required />
-                    <FormLabel htmlFor='for'>Email</FormLabel>
-                    <FormInput type='email' name='email' required />
-                    <FormLabel htmlFor='for'>Contact Number</FormLabel>
-                    <FormInput type='name' name='phone' required />
-                    <FormLabel htmlFor='for'>Check in Date</FormLabel>
-                    <FormInput type='date' name='CID' required />
-                    <FormLabel htmlFor='for'>Check out Date</FormLabel>
-                    <FormInput type='date' name='COD' required />
-                   
-                    <FormLabel htmlFor='for'>Number of Visitor
-                    <FormSelect name='visitor'>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    </FormSelect>
-                    </FormLabel>
-                    <FormInputBtn  name='submit' type='submit'>Book</FormInputBtn>
-                </Form>
-            </FormContent>
+          <Icon to="/">Genesis Farm</Icon>
+          <FormContent>
+            <Form action="" method="POST" onSubmit={sendEmail}>
+              <FormH1>Reservation Form</FormH1>
+              <FormLabel htmlFor="for">First Name</FormLabel>
+              <FormInput type="name" name="FN" required />
+              <FormLabel htmlFor="for">Last Name</FormLabel>
+              <FormInput type="name" name="LN" required />
+              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormInput type="email" name="email" required />
+              <FormLabel htmlFor="for">Contact Number</FormLabel>
+              <FormInput type="name" name="phone" required />
+              <FormLabel htmlFor="for">Check in Date</FormLabel>
+              <FormInput type="date" name="CID" required />
+              <FormLabel htmlFor="for">Check out Date</FormLabel>
+              <FormInput type="date" name="COD" required />
+
+              <FormLabel htmlFor="for">
+                Number of Visitor
+                <FormSelect name="visitor">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </FormSelect>
+              </FormLabel>
+              <FormButton name="submit" type="submit">
+                Book
+              </FormButton>
+            </Form>
+          </FormContent>
         </FormWrap>
-    </Container>
+      </Container>
     </>
   );
 };
