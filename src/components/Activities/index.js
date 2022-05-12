6dfcf0b20b729dc25a2bf8f9c701886dc39b1fc3
JoshMatthew <joshmatthewtalplacido@gmail.com>
2022-05-13 02:29:54 +0800

    fixed navbar

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Img } from '../InfoSection/InfoElements'
 
-function index() {
+function Activities() {
   return (
-    <div>index</div>
+    <div>Activities</div>
   )
 }
 
@@ -11,62 +11,56 @@ function index() {
 
  
  
- <div class="flip-main">
-   <div class="container">
-      <div class="row">
+//  <div class="flip-main">
+//    <div class="container">
+//       <div class="row">
          
-         <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                   <img src="img/1.jpg" alt="flip-card">
-                </div>
-                <div class="flip-card-back">
-                   <div class="text">
-                      <h3>Rashi Gupta</h3>
-                      <p>Web Designer</p>
-                   </div>
-                </div>
-            </div>
-         </div>
+//          <div class="flip-card">
+//             <div class="flip-card-inner">
+//                 <div class="flip-card-front">
+//                    <img src="img/1.jpg" alt="flip-card">
+//                 </div>
+//                 <div class="flip-card-back">
+//                    <div class="text">
+//                       <h3>Rashi Gupta</h3>
+//                       <p>Web Designer</p>
+//                    </div>
+//                 </div>
+//             </div>
+//          </div>
       
        
-         <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                   <img src="img/2.jpg" alt="flip-card">
-                </div>
-                <div class="flip-card-back">
-                   <div class="text">
-                      <h3>Upvesh Kumar</h3>
-                      <p>Web Designer</p>
-                   </div>
-                </div>
-            </div>
-         </div>
+//          <div class="flip-card">
+//             <div class="flip-card-inner">
+//                 <div class="flip-card-front">
+//                    <img src="img/2.jpg" alt="flip-card">
+//                 </div>
+//                 <div class="flip-card-back">
+//                    <div class="text">
+//                       <h3>Upvesh Kumar</h3>
+//                       <p>Web Designer</p>
+//                    </div>
+//                 </div>
+//             </div>
+//          </div>
         
-         <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                   <img src="img/3.jpg" alt="flip-card">
-                </div>
-                <div class="flip-card-back">
-                   <div class="text">
-                      <h3>Sonu Singh</h3>
-                      <p>Web Designer</p>
-                   </div>
-                </div>
-            </div>
-         </div>
+//          <div class="flip-card">
+//             <div class="flip-card-inner">
+//                 <div class="flip-card-front">
+//                    <img src="img/3.jpg" alt="flip-card">
+//                 </div>
+//                 <div class="flip-card-back">
+//                    <div class="text">
+//                       <h3>Sonu Singh</h3>
+//                       <p>Web Designer</p>
+//                    </div>
+//                 </div>
+//             </div>
+//          </div>
    
-      </div>
-   </div>
- </div>
+//       </div>
+//    </div>
+//  </div>
 
 
-
-
-
-
-
-
-export default index
+export default Activities
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -12,7 +12,7 @@ export const Nav = styled.nav`
     font-size: 1rem;
     position: sticky;
     top: 0;
-    z-index: 10;
+    z-index: 10000;
     
 
     @media screen and (max-width: 960px) {
